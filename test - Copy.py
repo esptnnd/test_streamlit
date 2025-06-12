fb45d2744845dc782b93f8c0bf4ab02dc6f1c423
esptnnd <ananda.sept@gmail.com>
2025-06-12 16:57:25 +0700

    Create test - Copy.py

---
+++ test - Copy.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+st.title("Title")
+st.header("Header")
+st.subheader("Subheader")
+st.text("Just some text")
+st.markdown("**Bold**, *Italic*, `Code`")
+
+
+
+hide_footer_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    .viewerBadge_link__1S137 {display: none;}
+    </style>
+"""
+
+st.markdown(hide_footer_style, unsafe_allow_html=True)
